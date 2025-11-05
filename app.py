ef70d4e30ecbb6c8819b13027058a63c6a6f7d57
tanyu-1535 <tanyu1535@foxmail.com>
2025-11-05 22:04:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -295,10 +295,9 @@ async def health_check():
 # Vercel需要这个变量
 app = app
 
-# 测试代码
+# -------------------- 本地调试 --------------------
 if __name__ == "__main__":
     import uvicorn
-
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
